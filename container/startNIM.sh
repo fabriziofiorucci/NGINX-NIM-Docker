d05981d12692e74000cb0e0042841e20a4810123
Fabrizio Fiorucci <fiorucci@oasi.asti.it>
2022-02-08 00:26:38 +0100

    Support for F5 Telemetry Tracker DATAPLANE_* naming change

--- container/startNIM.sh
+++ container/startNIM.sh
@@ -2,10 +2,10 @@
 
 if [ -f "/deployment/counter.enabled" ]
 then
-	export NGINX_CONTROLLER_TYPE=NGINX_INSTANCE_MANAGER
-	export NGINX_CONTROLLER_FQDN="http://127.0.0.1:11000"
-	export NGINX_CONTROLLER_USERNAME="username@domain"
-	export NGINX_CONTROLLER_PASSWORD="thepassword"
+	export DATAPLANE_TYPE=NGINX_INSTANCE_MANAGER
+	export DATAPLANE_FQDN="http://127.0.0.1:11000"
+	export DATAPLANE_USERNAME="username@domain"
+	export DATAPLANE_PASSWORD="thepassword"
 
 	python3 /deployment/app.py &
 fi
